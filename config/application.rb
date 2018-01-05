9e061fd974b61bb2a6f225d857b3387c00d7ea17
Victor Cedeno <vicedeno@gmail.com>
2018-01-05 17:54:12 -0300

    quit precompile assets

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Socialxapia
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.i18n.default_locale = :es
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/enviconfig.i18n.default_locale = :es
     #ronments/* take precedence over those specified here.
